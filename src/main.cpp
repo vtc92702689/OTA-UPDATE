86c19408e5b7cd640715816ed925c1a611d6d087
vtc92702689 <91804219+vtc92702689@users.noreply.github.com>
2024-12-12 14:46:24 +0700

    them chuc nang tao index.html trong Little FS

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@ IPAddress subnet(255, 255, 255, 0);     // Subnet Mask
 
 U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
+bool indexHtmlCreated = false; // Cờ để kiểm tra trạng thái tạo file index.html
+String resultCreateHtml;
+
 void createIndexHtml() {
     const char* indexHtml = R"rawliteral(
 <!DOCTYPE html>
@@ -40,6 +43,7 @@ void createIndexHtml() {
     File file = LittleFS.open("/index.html", "w");
     if (!file) {
         Serial.println("Failed to create index.html");
+        resultCreateHtml = "Failed to create";
         u8g2.clearBuffer();
         u8g2.drawStr(0, 10, "Failed to create");
         u8g2.drawStr(0, 30, "index.html");
@@ -49,59 +53,19 @@ void createIndexHtml() {
     file.print(indexHtml);
     file.close();
     
+    resultCreateHtml = "index.html created";
     Serial.println("index.html created");
     u8g2.clearBuffer();
     u8g2.drawStr(0, 10, "index.html created");
     u8g2.sendBuffer();
     delay(5000); // Hiển thị thông báo 5 giây trước khi vào loop
+    
+    indexHtmlCreated = true; // Đặt cờ khi tạo file thành công
 }
 
 void setup() {
   Serial.begin(115200);
-  
-  // Cấu hình địa chỉ IP tĩnh
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("Failed to configure STA");
-  }
-
-  WiFi.begin(ssid, password);
-  
-  // Hiển thị quá trình kết nối WiFi lên màn hình OLED
   u8g2.begin();
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.drawStr(0, 10, "Connecting to WiFi");
-  u8g2.sendBuffer();
-
-  static int dotCount = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); // Giảm thời gian delay để tăng tốc kết nối
-    Serial.print(".");
-
-    // Cập nhật màn hình OLED với dấu chấm
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "Connecting to WiFi");
-    String dots = String(".");
-    for (int i = 0; i < dotCount; i++) {
-      dots += ".";
-    }
-    u8g2.drawStr(0, 30, dots.c_str());
-    u8g2.sendBuffer();
-    dotCount = (dotCount + 1) % 4; // Tạo hiệu ứng xoay vòng
-  }
-  
-  Serial.println("\nWiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.drawStr(0, 10, "WiFi connected");
-  u8g2.drawStr(0, 30, "IP address:");
-  String url = WiFi.localIP().toString();
-  u8g2.drawStr(0, 50, url.c_str());
-  u8g2.sendBuffer();
   
   // Mount LittleFS và tạo tệp index.html
   if (!LittleFS.begin()) {
@@ -113,61 +77,108 @@ void setup() {
     return;
   }
   createIndexHtml(); // Tạo tệp index.html
-
-  ArduinoOTA.setPassword(ota_password); // Thiết lập mật khẩu cho OTA
-  ArduinoOTA.begin();
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { 
-      type = "filesystem";
+  
+  // Chỉ tiến hành cấu hình WiFi nếu tạo file thành công
+  if (indexHtmlCreated) {
+    // Cấu hình địa chỉ IP tĩnh
+    if (!WiFi.config(local_IP, gateway, subnet)) {
+      Serial.println("Failed to configure STA");
     }
-    Serial.println("Start updating " + type);
-  });
-
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    
+    WiFi.begin(ssid, password);
+    
+    // Hiển thị quá trình kết nối WiFi lên màn hình OLED
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "Updating...");
-    u8g2.drawStr(0, 20, ("Progress: " + String((progress / (total / 100))) + "%").c_str());
+    u8g2.drawStr(0, 10, "Connecting to WiFi");
     u8g2.sendBuffer();
-  });
 
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    String errorMessage;
+    static int dotCount = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500); // Giảm thời gian delay để tăng tốc kết nối
+      Serial.print(".");
 
-    if (error == OTA_AUTH_ERROR) {
-      errorMessage = "Auth Failed";
-      Serial.println(errorMessage);
-    } else if (error == OTA_BEGIN_ERROR) {
-      errorMessage = "Begin Failed";
-      Serial.println(errorMessage);
-    } else if (error == OTA_CONNECT_ERROR) {
-      errorMessage = "Connect Failed";
-      Serial.println(errorMessage);
-    } else if (error == OTA_RECEIVE_ERROR) {
-      errorMessage = "Receive Failed";
-      Serial.println(errorMessage);
-    } else if (error == OTA_END_ERROR) {
-      errorMessage = "End Failed";
-      Serial.println(errorMessage);
+      // Cập nhật màn hình OLED với dấu chấm
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.drawStr(0, 10, "Connecting to WiFi");
+      String dots = String(".");
+      for (int i = 0; i < dotCount; i++) {
+        dots += ".";
+      }
+      u8g2.drawStr(0, 30, dots.c_str());
+      u8g2.sendBuffer();
+      dotCount = (dotCount + 1) % 4; // Tạo hiệu ứng xoay vòng
     }
+    
+    Serial.println("\nWiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
-    // Hiển thị lỗi lên màn hình OLED
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "OTA Error:");
-    u8g2.drawStr(0, 20, errorMessage.c_str());
+    u8g2.drawStr(0, 11, "WiFi connected");
+    u8g2.drawStr(0, 22, "IP address:");
+    String url = WiFi.localIP().toString();
+    u8g2.drawStr(0, 33, url.c_str());
+    u8g2.drawStr(0, 44, resultCreateHtml.c_str());
     u8g2.sendBuffer();
-  });
+
+    ArduinoOTA.setPassword(ota_password); // Thiết lập mật khẩu cho OTA
+    ArduinoOTA.begin();
+
+    ArduinoOTA.onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else { 
+        type = "filesystem";
+      }
+      Serial.println("Start updating " + type);
+    });
+
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.drawStr(0, 11, "Updating...");
+      u8g2.drawStr(0, 22, ("Progress: " + String((progress / (total / 100))) + "%").c_str());
+      u8g2.sendBuffer();
+    });
+
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      String errorMessage;
+
+      if (error == OTA_AUTH_ERROR) {
+        errorMessage = "Auth Failed";
+        Serial.println(errorMessage);
+      } else if (error == OTA_BEGIN_ERROR) {
+        errorMessage = "Begin Failed";
+        Serial.println(errorMessage);
+      } else if (error == OTA_CONNECT_ERROR) {
+        errorMessage = "Connect Failed";
+        Serial.println(errorMessage);
+      } else if (error == OTA_RECEIVE_ERROR) {
+        errorMessage = "Receive Failed";
+        Serial.println(errorMessage);
+      } else if (error == OTA_END_ERROR) {
+        errorMessage = "End Failed";
+        Serial.println(errorMessage);
+      }
+
+      // Hiển thị lỗi lên màn hình OLED
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.drawStr(0, 11, "OTA Error:");
+      u8g2.drawStr(0, 22, errorMessage.c_str());
+      u8g2.sendBuffer();
+    });
+  }
 }
 
 void loop() {
@@ -178,8 +189,8 @@ void loop() {
     Serial.println("WiFi disconnected. Reconnecting...");
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "WiFi disconnected");
-    u8g2.drawStr(0, 20, "Reconnecting...");
+    u8g2.drawStr(0, 11, "WiFi disconnected");
+    u8g2.drawStr(0, 22, "Reconnecting...");
     u8g2.sendBuffer();
 
     // Kết nối lại WiFi
@@ -208,8 +219,10 @@ void loop() {
 
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "WiFi reconnected");
-    u8g2.drawStr(0, 20, WiFi.localIP().toString().c_str());
+    u8g2.drawStr(0, 11, "WiFi reconnected");
+    u8g2.drawStr(0, 22, "IP address:");
+    u8g2.drawStr(0, 33, WiFi.localIP().toString().c_str());
+    u8g2.drawStr(0, 44, resultCreateHtml.c_str());
     u8g2.sendBuffer();
   }
 
